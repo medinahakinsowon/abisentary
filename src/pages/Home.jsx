43e33809cdc91b746d2fbedb1036c9eefa4a6fe6
medinah <ethusiast.medinah@cloudethusiat.com>
2026-08-25 15:19:03 +0100

    imported radar map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ import SecurityFramework from "../components/SecurityFramework";
 import ResourceDownload from "../components/ResourcesDownload";
 import SecurityadvisorCTA from "../components/SecurityadvisorCTA";
 import BacktoTop from "../components/BacktoTop";
+import RadarMap from "../components/RadarMap";
 
 const sectors = [
   "Finance & Fintech",
